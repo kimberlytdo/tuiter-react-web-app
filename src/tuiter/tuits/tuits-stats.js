f004020d3981d14ba0bfd86213997d37b1c7e07f
Kimberly Do <kimberlydo@outlook.com>
2023-04-23 13:26:52 +0200

    a8 redo sad

--- src/tuiter/tuits/tuits-stats.js
+++ src/tuiter/tuits/tuits-stats.js
@@ -1,41 +1,73 @@
 import React from "react";
-import {useDispatch} from "react-redux";
-import {updateTuitThunk} from "../../services/tuits-thunks.js";
+import { useDispatch } from "react-redux";
+import { updateTuitThunk } from "../../services/tuits-thunks.js";
 
-const TuitStats = ({post}) => {
+const TuitStats = ({
+   tuit = {
+       liked: false,
+       disliked: false,
+       replies: 0,
+       retuits: 0,
+       likes: 0,
+       dislikes: 0,
+   },
+}) => {
     const dispatch = useDispatch();
-    let heart_icon;
-    if (post.liked){
-        heart_icon = <i className="fa fa-heart ps-1 pt-3" style = {{"color":"red"}}></i>;
-    } else{
-        heart_icon = <i className="fa fa-heart"></i>;
-    }
-    let dislike_icon = <i className="bi bi-hand-thumbs-down"></i>
+    const handleLikeClick = () => {
+        const newLikesCount = tuit.liked ? tuit.likes - 1 : tuit.likes + 1;
+        dispatch(
+            updateTuitThunk({
+                ...tuit,
+                liked: !tuit.liked,
+                likes: newLikesCount,
+            })
+        );
+    };
+    const handleDislikeClick = () => {
+        const newDislikesCount = tuit.disliked
+            ? tuit.dislikes - 1
+            : tuit.dislikes + 1;
+        dispatch(
+            updateTuitThunk({
+                ...tuit,
+                disliked: !tuit.disliked,
+                dislikes: newDislikesCount,
+            })
+        );
+    };
     return (
-        <div>
-            <i className={"fa fa-comment ps-1 pt-3 text-secondary"}/>
-            <label className=" ps-1 pe-5 pt-3 text-secondary" >{post.replies}</label>
-
-            <i className={"fa fa-retweet ps-1 pt-3 text-secondary"}/>
-            <label className=" ps-1 pe-5 pt-3 text-secondary">{post.retuits}</label>
-
-            <>
-                <i onClick={() => dispatch(updateTuitThunk({...post,
-                    likes: post.likes + 1,
-                    liked: true}))}>{heart_icon}</i>
-                <span className = "ps-1 pe-5 pt-3 text-secondary"> {post.likes}</span>
-            </>
-
-            <>
-                <i onClick={() => dispatch(updateTuitThunk({...post,
-                    dislikes: post.dislikes + 1,
-                    disliked: true}))}>{dislike_icon}</i>
-                <span className = "ps-1 pe-5 pt-3 text-secondary"> {post.dislikes}</span>
-            </>
-
-            <i className={"fa fa-external-link ps-1 pt-3 text-secondary"}/>
-        </div>
-    )
+        <ul className="d-flex justify-content-around align-items-center">
+            <li>
+                <i className="bi bi-chat"></i> {tuit.replies}
+            </li>
+            <li>
+                <i className="bi bi-arrow-clockwise"></i> {tuit.retuits}
+            </li>
+            <li>
+                <i
+                    onClick={handleLikeClick}
+                    className={`bi ${
+                        tuit.liked ? "bi-heart-fill text-danger" : "bi-heart"
+                    }`}
+                ></i>{" "}
+                {tuit.likes}
+            </li>
+            <li>
+                <i
+                    onClick={handleDislikeClick}
+                    className={`bi ${
+                        tuit.disliked
+                            ? "bi-hand-thumbs-down-fill text-secondary"
+                            : "bi-hand-thumbs-down"
+                    }`}
+                ></i>{" "}
+                {tuit.dislikes}
+            </li>
+            <li>
+                <i className="bi bi-share"></i>
+            </li>
+        </ul>
+    );
 };
 
 export default TuitStats;
--- src/tuiter/tuits/tuits.js
+++ src/tuiter/tuits/tuits.js
@@ -1,77 +1,41 @@
 import React from "react";
-import {useDispatch} from "react-redux";
-import {deleteTuitThunk} from "../../services/tuits-thunks.js";
+import TuitStats from "./tuit-stats.js";
+import "../../styles/styles.css"
+import { useDispatch } from "react-redux";
+import { deleteTuitThunk } from "../../services/tuits-thunks.js";
 
-const TuitItem = (
-    {
-        tuit = {
-            _id: 234,
-            topic: "Space",
-            userName: "SpaceX",
-            time: "2h",
-            title: "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-            image: "spacex.jpeg",
-            liked: true,
-            replies: 123,
-            retuits: 432,
-            likes: 2345,
-            handle: "@spacex",
-            tuit: "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
-        }
-    }
-) => {
-
+const TuitItem = ({
+  tuit = {
+      userName: undefined,
+      time: undefined,
+      image: undefined,
+      handle: undefined,
+      tuit: "",
+  },
+}) => {
     const dispatch = useDispatch();
-    const deleteTuitHandler = (id) => {
-        dispatch(deleteTuitThunk(id));
-    }
-
-    return(
-        <li className="list-group-item border-0">
-            <div className="row">
-                <div className="col-1 ">
-                    <img className="float-end rounded-pill" src={`../images/${tuit.image}`} height="40px"/>
-                    {/*<img width={70} className="float-end rounded-3" src={`/images/${post.image}`}/>*/}
-                </div>
-                <div className="col-11 ">
-                    <div className="fw-bolder">
-                        {tuit.userName} <i className="fas fa-check-circle ps-1"></i><i
-                        className="bi bi-check-circle-fill text-primary"></i>&nbsp;&nbsp;
-                        <span className="text-secondary fw-normal wd-text-post-small">{tuit.subName} -  {tuit.time}</span>
+    return (
+        <div className="list-group-item d-flex">
+            <img
+                className="wd-profile-pic rounded-circle"
+                src={`/images/${tuit.image}`}
+                alt={tuit.userName}
+            />
+            <div className="ps-3 w-100">
+                <ul className="pb-3">
+                    <li>
+                        <span className="fw-bold">{tuit.username}</span>{" "}
+                        <i className="bi bi-patch-check-fill text-primary"></i>{" "}
+                        {tuit.handle} · {tuit.time}
                         <i className="bi bi-x-lg float-end"
-                           onClick={() => deleteTuitHandler(tuit._id)}></i>
-                    </div>
-                    <div className="pt-1 pb-2">
-                        {tuit.tuit}
-
-                    </div>
-                    <div className="border rounded">
-                        <div className="text-secondary wd-text-post-small">
-                            {tuit.link}
-                        </div>
-                    </div>
-
-                    <div>
-
-                        <i className="far fa-comment ps-1 pt-3 text-secondary"></i><i className="bi bi-chat"></i>
-                        <label className=" ps-1 pe-5 pt-3 text-secondary">{tuit.replies}</label>
-
-                        <i className="fas fa-retweet ps-1 pt-3 text-secondary"></i><i
-                        className="bi bi-arrow-repeat"></i>
-                        <label className=" ps-1 pe-5 pt-3 text-secondary">{tuit.retuits}</label>
-
-                        <span><i className="far fa-heart ps-1 pt-3 text-secondary"></i>
-                                  <b>{ tuit.liked ?
-                                <i className="bi bi-heart-fill text-danger"></i> : <i className="bi bi-heart"></i>}</b>
-                            <label className=" ps-1 pe-5 pt-3 text-secondary">{tuit.likes}</label></span>
-
-                        <i className="bi bi-share"></i>
-                    </div>
-                </div>
-
+                           onClick={() => dispatch(deleteTuitThunk(tuit._id))}></i>
+                    </li>
+                    <li>{tuit.tuit}</li>
+                </ul>
+                <TuitStats tuit={tuit} />
             </div>
-        </li>
+        </div>
     );
 };
-export default TuitItem;
 
+export default TuitItem;
