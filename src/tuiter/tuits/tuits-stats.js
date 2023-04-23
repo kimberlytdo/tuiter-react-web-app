5a916fead8895b8ce24530fcb8c0b6c3b6aafabb
Kimberly Do <kimberlydo@outlook.com>
2023-04-23 13:32:08 +0200

    tuit stats

--- src/tuiter/tuits/tuits-stats.js
+++ src/tuiter/tuits/tuits-stats.js
@@ -2,40 +2,80 @@ import React from "react";
 import {useDispatch} from "react-redux";
 import {updateTuitThunk} from "../../services/tuits-thunks.js";
 
-const TuitStats = ({post}) => {
-    const dispatch = useDispatch();
-    let heart_icon;
-    if (post.liked){
-        heart_icon = <i className="fa fa-heart ps-1 pt-3" style = {{"color":"red"}}></i>;
-    } else{
-        heart_icon = <i className="fa fa-heart"></i>;
+const TuitStats  = (
+    {
+        tuit = {
+            _id: 234,
+            topic: "Space",
+            userName: "SpaceX",
+            time: "2h",
+            title: "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+            image: "spacex.jpeg",
+            liked: true,
+            replies: 123,
+            retuits: 432,
+            likes: 2345,
+            handle: "@spacex",
+            tuit: "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
+        }
     }
-    let dislike_icon = <i className="bi bi-hand-thumbs-down"></i>
+) => {
+    const dispatch = useDispatch();
+    const handleLikeClick = () => {
+        const newLikesCount = tuit.liked ? tuit.likes - 1 : tuit.likes + 1;
+        dispatch(
+            updateTuitThunk({
+                ...tuit,
+                liked: !tuit.liked,
+                likes: newLikesCount,
+            })
+        );
+    };
+    const handleDislikeClick = () => {
+        const newDislikesCount = tuit.disliked
+            ? tuit.dislikes - 1
+            : tuit.dislikes + 1;
+        dispatch(
+            updateTuitThunk({
+                ...tuit,
+                disliked: !tuit.disliked,
+                dislikes: newDislikesCount,
+            })
+        );
+    };
     return (
-        <div>
-            <i className={"fa fa-comment ps-1 pt-3 text-secondary"}/>
-            <label className=" ps-1 pe-5 pt-3 text-secondary" >{post.replies}</label>
-
-            <i className={"fa fa-retweet ps-1 pt-3 text-secondary"}/>
-            <label className=" ps-1 pe-5 pt-3 text-secondary">{post.retuits}</label>
-
-            <>
-                <i onClick={() => dispatch(updateTuitThunk({...post,
-                    likes: post.likes + 1,
-                    liked: true}))}>{heart_icon}</i>
-                <span className = "ps-1 pe-5 pt-3 text-secondary"> {post.likes}</span>
-            </>
-
-            <>
-                <i onClick={() => dispatch(updateTuitThunk({...post,
-                    dislikes: post.dislikes + 1,
-                    disliked: true}))}>{dislike_icon}</i>
-                <span className = "ps-1 pe-5 pt-3 text-secondary"> {post.dislikes}</span>
-            </>
-
-            <i className={"fa fa-external-link ps-1 pt-3 text-secondary"}/>
-        </div>
-    )
+        <ul className="d-flex justify-content-around align-items-center">
+            <li>
+                <i className="bi bi-chat"></i> {tuit.replies}
+            </li>
+            <li>
+                <i className="bi bi-arrow-clockwise"></i> {tuit.retuits}
+            </li>
+            <li>
+                <i
+                    onClick={handleLikeClick}
+                    className={`bi ${
+                        tuit.liked ? "bi-heart-fill text-danger" : "bi-heart"
+                    }`}
+                ></i>{" "}
+                {tuit.likes}
+            </li>
+            <li>
+                <i
+                    onClick={handleDislikeClick}
+                    className={`bi ${
+                        tuit.disliked
+                            ? "bi-hand-thumbs-down-fill text-secondary"
+                            : "bi-hand-thumbs-down"
+                    }`}
+                ></i>{" "}
+                {tuit.dislikes}
+            </li>
+            <li>
+                <i className="bi bi-share"></i>
+            </li>
+        </ul>
+    );
 };
 
 export default TuitStats;
