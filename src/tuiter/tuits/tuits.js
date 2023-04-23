20ae7bf9db472e864cec38543b7c6884be7c5926
Kimberly Do <kimberlydo@outlook.com>
2023-04-23 13:29:52 +0200

    tuits

--- src/tuiter/tuits/tuits.js
+++ src/tuiter/tuits/tuits.js
@@ -1,20 +1,30 @@
 import React from "react";
-import TuitStats from "./tuit-stats.js";
-import "../../styles/styles.css"
-import { useDispatch } from "react-redux";
-import { deleteTuitThunk } from "../../services/tuits-thunks.js";
+import {useDispatch} from "react-redux";
+import {deleteTuitThunk} from "../../services/tuits-thunks.js";
+import TuitStats from "./tuits-stats.js";
 
-const TuitItem = ({
-  tuit = {
-      userName: undefined,
-      time: undefined,
-      image: undefined,
-      handle: undefined,
-      tuit: "",
-  },
-}) => {
+const TuitItem = (
+    {
+        tuit = {
+            _id: 234,
+            topic: "Space",
+            userName: "SpaceX",
+            time: "2h",
+            title: "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+            image: "spacex.jpeg",
+            liked: true,
+            replies: 123,
+            retuits: 432,
+            likes: 2345,
+            handle: "@spacex",
+            tuit: "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
+        }
+    }
+) => {
+
     const dispatch = useDispatch();
-    return (
+
+    return(
         <div className="list-group-item d-flex">
             <img
                 className="wd-profile-pic rounded-circle"
@@ -37,5 +47,5 @@ const TuitItem = ({
         </div>
     );
 };
-
 export default TuitItem;
+
